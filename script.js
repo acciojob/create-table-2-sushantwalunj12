f8887e35b5a289b9a89a8382472978cd5d258ac0
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-04-12 18:25:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+//your code here
 function createTable() {
-    //Write your code here
+    rn = window.prompt("Input rows", 3);
+    cn = window.prompt("Input columns",3);
   
+    for(var r=0;r<parseInt(rn,10);r++) {
+        var x=document.getElementById('myTable').insertRow(r);
+        for(var c=0;c<parseInt(cn,10);c++) {
+            var y=  x.insertCell(c);
+            y.innerHTML="Row-"+r+" Column-"+c; 
+        }
+    }
 }
